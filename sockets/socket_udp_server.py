ab58f3369dcd2cc3e93be24b5cace8fb94421da1
Gabrielle Torres Machado <gabrielletrr17@gmail.com>
2025-11-29 13:01:02 -0300

    Update socket_udp_server.py

--- sockets/socket_udp_server.py
+++ sockets/socket_udp_server.py
@@ -1,52 +1,57 @@
-# Importando a biblioteca SOCKET
 import socket
 
 # ----------------------------------------------------------------------
 HOST_IP_SERVER  = ''              # Definindo o IP do servidor
 HOST_PORT       = 50000           # Definindo a porta
+
+BUFFER_SIZE     = 10              # Tamanho do buffer
 CODE_PAGE       = 'utf-8'         # Definindo a página de 
                                   # codificação de caracteres
-BUFFER_SIZE     = 512             # Tamanho do buffer
 # ----------------------------------------------------------------------
 
 # Criando o socket (socket.AF_INET -> IPV4 / socket.SOCK_DGRAM -> UDP)
 sockServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Ligando o socket à porta
-sockServer.bind((HOST_IP_SERVER, HOST_PORT)) 
+sockServer.bind( (HOST_IP_SERVER, HOST_PORT) ) 
 
-# Definindo um timeout de 0.5 segundos para o socket
+# Definindo um timeout (tempo de vida) para o socket
 sockServer.settimeout(0.5)
 
 print('\nRecebendo Mensagens...')
-print('Pressione CTRL+C para encerrar o servidor...\n')
-print('-' * 100 + '\n')
+print('Pressione CTRL+C para sair do servidor...\n')
+print('-'*100 + '\n')
 
 try:
-    while True:
-        try:
-            # Recebendo os dados do cliente
-            byteMensagem, tuplaCliente = sockServer.recvfrom(BUFFER_SIZE)
+   while True:
+      try:
+         # Recebendo os dados do cliente
+         byteMensagem, tuplaCliente = sockServer.recvfrom(BUFFER_SIZE)
+         intTamanhoMensagem = int(byteMensagem.decode(CODE_PAGE))
+         if intTamanhoMensagem > BUFFER_SIZE: BUFFER_SIZE = intTamanhoMensagem
+         byteMensagem, tuplaCliente = sockServer.recvfrom(BUFFER_SIZE)
+      except socket.timeout:
+         continue
+      else:
+         # Obtendo o nome (HOST) do cliente
+         strNomeHost = socket.gethostbyaddr(tuplaCliente[0])[0].split('.')[0].upper()
 
-        except socket.timeout:
-            continue
+         # Imprimindo a mensagem recebida convertendo de bytes para string
+         strMensagem = byteMensagem.decode(CODE_PAGE)
+         print(f'{tuplaCliente} -> {strNomeHost}: {strMensagem}')
 
-        else:
-            
-            # Imprimindo a mensagem recebida convertendo de bytes para string
-            print(f'{tuplaCliente} -> {byteMensagem.decode(CODE_PAGE)}')
-            print('Mensagem recebida com sucesso!')
+         strMensagem = strMensagem[::-1].encode(CODE_PAGE)
 
-            resposta = byteMensagem
-            sockServer.sendto(resposta, tuplaCliente)
-            print('Mensagem reenviada com sucesso!')
+         # Enviando o tamanho da mensagem ao servidor
+         bytesTamanhoMensagem = str(len(strMensagem)).encode(CODE_PAGE)
+         sockServer.sendto(bytesTamanhoMensagem, tuplaCliente)
+         
+         # Devolvendo uma mensagem ao cliente
+         sockServer.sendto(strMensagem, tuplaCliente)
 
 except KeyboardInterrupt:
-    print('\n\nAVISO: Interrupção detectada (CTRL + C). Encerrando servidor...')
-
+   print('\nAVISO: Foi Pressionado CTRL+C...\nSaindo do Servidor...\n\n')
 finally:
-    # Fechando o socket
-    sockServer.close()
-    print('Servidor finalizado com sucesso.')
-    
-    #sem alterrar o buffer comi fazer para receber uma mesg maior que ele 
+   # Fechando o socket
+   sockServer.close()
+   print('Servidor finalizado com sucesso...\n\n')
