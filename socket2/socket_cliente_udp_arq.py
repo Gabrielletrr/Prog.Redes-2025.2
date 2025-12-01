0d452b087f3591995be5a628957744978e9b1462
Gabrielletrr <gabrielletrr17@gmail.com>
2025-12-01 00:20:07 -0300

    .

--- socket2/socket_cliente_udp_arq.py
+++ socket2/socket_cliente_udp_arq.py
@@ -14,34 +14,40 @@ print('\n\nPara sair digite SAIR...\n\n')
 
 while True:
   # Informando a mensagem a ser enviada para o servidor
-  strMensagem = input('Digite a mensagem: ')
+  NomeArquivo = input('Digite o nome do arquivo: ')
 
   # Saindo do Cliente quando digitar SAIR
-  if strMensagem.lower().strip() == 'sair': break
+  if NomeArquivo.lower().strip() == 'sair': break
 
-  # Enviando tamanho da mensagem
-  dados = strMensagem.encode(CODE_PAGE)
-  TamanhoMensagem = len(dados)
-  sockClient.sendto(str(TamanhoMensagem).encode(CODE_PAGE), TUPLA_SERVIDOR)
-  print('Tamanho da mensagem', TamanhoMensagem)
+  # Envia o nome do arquivo para o servidor
+  sockClient.sendto(NomeArquivo.encode(CODE_PAGE), TUPLA_SERVIDOR)
 
-  
-  # Enviando a mensagem fragmentada
-  i = 0
-  while i < TamanhoMensagem:
-    fragmento = strMensagem[i:i+BUFFER_SIZE]
-    sockClient.sendto((str(fragmento).encode(CODE_PAGE)), TUPLA_SERVIDOR)
-    i += BUFFER_SIZE  
+  # Recebendo a confirmação
+  ConfirmacaoBytes, TUPLA_SERVIDOR = sockClient.recvfrom(BUFFER_SIZE)
+  Confirmacao = ConfirmacaoBytes.decode(CODE_PAGE)
 
-  # Recebendo resposta do servidor 
-  Resposta = ''
-  Partes = 0
-  while Partes < TamanhoMensagem:
-    bytesMensagem, end_server = sockClient.recvfrom(BUFFER_SIZE)
-    MensagemResposta = bytesMensagem.decode(CODE_PAGE)
-    Resposta += MensagemResposta
-    Partes += len(MensagemResposta)
-  print(f'Resposta Servidor: {Resposta}')
+  if ConfirmacaoBytes == 'ERRO':
+    print('Arquivo não encontrado')
+  else:
+    print('Iniciando download ...')
+
+    # Recebendo o tamanho do arquivo
+    TamanhoBytes, TUPLA_SERVIDOR = sockClient.recvfrom(BUFFER_SIZE)
+    TamanhoArquivo = int(TamanhoBytes.decode(CODE_PAGE)) 
+    print(f'Tamanho total do arquivo {TamanhoArquivo}')
+
+    # Recebendo resposta do servidor 
+    Resposta = ''
+    Partes = 0
+    while Partes < TamanhoArquivo:
+      bytesMensagem, end_server = sockClient.recvfrom(BUFFER_SIZE)
+      Resposta += bytesMensagem
+      Partes += len(Resposta)
+    print(f'Resposta Servidor: {Resposta}')
+
+    # Salvando o arquivo
+    with open('Recebido_' + NomeArquivo, 'wb') as f:
+      f.write(Resposta)
 
 # Fechando o socket
 sockClient.close()
